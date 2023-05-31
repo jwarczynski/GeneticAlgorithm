19fdd75ccac60863ff1c90609eab935500845698
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznna.pl>
2023-05-31 10:34:32 +0200

    add header file for parallel.cpp

---
+++ headers/parallel.h
@@ -0,0 +1,27 @@
+#ifndef PARALLEL_H
+#define PARALLEL_H
+
+#include "common.h"
+#include "util.h"
+
+namespace parallel {
+
+	int fittest(const int *chromosome);
+	int fittest(vector<int> *chromosome);
+	int maxDegree();
+	void translate(string name);
+	vector<pair<vector<int> *, int> *> *generateSmallSample();
+	int geneticAlg(vector<pair<vector<int> *, int> *> *sample, unsigned int iterations);
+	vector<pair<vector<int> *, int> *> *devaluate(vector<pair<vector<int> *, int> *> *population, int maxColors);
+	vector<int> *mate(vector<int> *mother, vector<int> *father, int maxColors);
+	vector<int> *minimalizeColors(vector<int> *chromosome, int maxColors);
+	int colorCount(vector<pair<vector<int> *, int> *> *population);
+	int colorCount(vector<int> *chromosome);
+	void mutate(vector<int> *chromosome, int maxColor, int a);
+	vector<vector<int> *> *crossover(vector<int> *first, vector<int> *second);
+	vector<vector<int> *> *newPop(vector<vector<int> *> *population);
+	vector<pair<vector<int> *, int> *> *generatePopulation(int maxDegree);
+}
+
+#endif
+
