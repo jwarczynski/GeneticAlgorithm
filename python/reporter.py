60330b55fe99a983751f43d722c0a14c228da4c3
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznna.pl>
2023-06-02 00:44:50 +0200

    updated python script for saving results to csv

--- python/reporter.py
+++ python/reporter.py
@@ -4,27 +4,29 @@ import csv
 import pandas as pd
 
 input_folder = "../input"
+input_folder = "input"
 bin_folder = "../bin"
-output_file = "../out/output.csv"
+bin_folder = "bin"
+output_file = "../out/output2.csv"
+output_file = "out/output2.csv"
 
 files = os.listdir(input_folder)
-print(files)
+sorted_files = sorted(files, key=int)
+print(sorted_files)
 
 data = pd.DataFrame(index=files, columns=["OpenMpTime", "SequentialTime", "CudaTime", "OpenMpRes", "SequentialRes", "CudaRes"])
 
 with open(output_file, mode='w', newline='') as output:
     writer = csv.writer(output)
-    headers = ["Vertices", "SequentialTime", "OpenMpTime", "CudaTime", "SequentialRes", "OpenMpRes", "CudaRes"]
+    headers = ["Vertices", "CudaTime", "OpenMpTime", "SequentialTime", "CudaRes", "OpenMpRes", "SequentialRes"]
     writer.writerow(headers)
-    for file in reversed(files):
-        # Wywołanie pliku main
-        main_output = subprocess.check_output([os.path.join(bin_folder, "main"), os.path.join(input_folder, file), "20"])
+    for file in sorted_files:
+        if (int(file) < 100):
+            continue
+        print(f'filename: {file}')
+        main_output = subprocess.check_output([os.path.join(bin_folder, "main"), os.path.join(input_folder, file), "40"])
         main_output = main_output.decode().strip().split("\n")
         
-        # Wywołanie pliku cuda
-        cuda_output = subprocess.check_output([os.path.join(bin_folder, "cuda"), os.path.join(input_folder, file), "20"])
-        cuda_output = cuda_output.decode().strip().split()
-
-        row = [file, main_output[1].split()[0], main_output[0].split()[0], cuda_output[0], main_output[1].split()[1], main_output[0].split()[1], cuda_output[1]]
+        row = [file, main_output[0].split()[0], main_output[1].split()[0], main_output[2], main_output[0].split()[1], main_output[1].split()[1], main_output[1]]
         writer.writerow(row)
-        print(row)
+        print("results: ", row)
